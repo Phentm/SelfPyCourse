cc355744bc70ff60c57c93a7deeeb6ea0e4a4d17
Phentm <www.mamamax99.yakovlev@gmail.com>
2026-01-04 09:36:33 +0200

    Refactored Unit 5 hangman exercise and added main() func syntax

--- Hangman_Project/ex_5.5.1.py
+++ Hangman_Project/ex_5.5.1.py
@@ -1,11 +1,15 @@
+def main():
+    print(is_valid_input('a'))  # True
+    print(is_valid_input('A'))  # True
+    print(is_valid_input('$'))  # False
+    print(is_valid_input("ab"))  # False
+    print(is_valid_input("app$"))  # False
+
 def is_valid_input(letter_guessed):
     letter_guessed = str(letter_guessed)
     return (len(letter_guessed) == 1
             and letter_guessed.isalpha()
             and letter_guessed.isascii())
 
-print(is_valid_input('a'))
-print(is_valid_input('A'))
-print(is_valid_input('$'))
-print(is_valid_input("ab"))
-print(is_valid_input("app$"))
+if __name__ == "__main__":
+    main()
